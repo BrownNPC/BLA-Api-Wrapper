f530abc0c40ac1c5e4d9403b6830b6220f636d7a
Deaddogfuneral <deaddogfuneral@gmail.com>
2023-03-23 17:53:12 +0500

    add search by student id feature

--- BlaApi/diary.py
+++ BlaApi/diary.py
@@ -8,9 +8,20 @@ class Diary:
         self.diary_list = self.client.diary_list()
 
     
+    def search_by_student_id(self, student_id):
+        diary = self.client.diary_list()
 
-    def search_by_student_id(self, student_id= None):
-        #If student id is not passed, use the first one
+        # make a POST to diary data endpoint 
+        # if student id is a match and return
+        # the diaries as a list
 
-        return f"student id is {student_id}"
+        output = [] # initialize
+
+        for d in diary:
+            if d["studentId"] == student_id:
+                notification_id = d['id']
+                response = self.client.get_diary_data(notification_id=notification_id)
+                output.append(response)
+
+        return output
     
