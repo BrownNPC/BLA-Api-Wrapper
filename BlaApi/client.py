238cccd98bbb4205e455303f6b1608bd8321806e
Deaddogfuneral <deaddogfuneral@gmail.com>
2023-03-23 17:53:39 +0500

    new commit

--- BlaApi/client.py
+++ BlaApi/client.py
@@ -5,10 +5,10 @@ import datetime
 from fake_useragent import UserAgent as ua
 
 class Client():
-	client = httpx.Client() #instanciate 
-	#!BLA credentials must be passed in as string
+	client = httpx.Client() # instanciate 
+	#! BLA credentials must be passed in as string
 	def __init__(self, username: str, password: str):
-		#Random UserAgent
+		# Random UserAgent
 		self.headers = {'UserAgent': str(ua().chrome)}
 		self.username = username
 		self.password = password
@@ -21,33 +21,33 @@ class Client():
 		
 		# Send POST request to API endpoint with headers
 		response = self.client.post(api_url, headers=self.headers)
-		#!Raise an error if POST fails
+		#! Raise an error if POST fails
 		response.raise_for_status()
 		
-		#Parse JSON response
+		# Parse JSON response
 		data = json.loads(response.content)['data']
 
-		#Retrieve access token & student info from JSON response
+		# Retrieve access token & student info from JSON response
 		token = data.get('accessToken')
 		if token is None:
 			raise ValueError("Access token not found in response.")
 		student_info = data.get('students')
 
-		#Return student ids and student names as lists
+		# Return student ids and student names as lists
 		student_ids = [] #Initialize
 		student_names = [] #Initialize
 
 		for s in student_info:
-			#Return student ids as list
+			# Return student ids as list
 			id = s.get('id')
 			student_ids.append(id)
 
-			#Return student name as list
+			# Return student name as list
 			name = s.get('studentName')
 			student_names.append(name)
 
 
-		#Return retrieved data as dictionary
+		# Return retrieved data as dictionary
 		output = {
 			'token': token,
 			'student_id': student_ids,
@@ -63,15 +63,18 @@ class Client():
 		
 		# Send POST request to API endpoint with headers
 		response = self.client.post(api_url, headers=self.headers)
-		#!Raise an error if POST fails
+
+		#! Raise an error if POST fails
 		response.raise_for_status()
 		
-		#Retreive diary list from JSON data
+		# Retreive diary list from JSON response
 		data = json.loads(response.content)['data']
 		
 		return data
 
 
+	#Retreive current date in a format that the api requires. 
+	#EXAMPLE: "Tue, 9/11/2001"
 	def get_current_date(self):
 		# Get current date and day of week
 		current_date = datetime.datetime.today()
@@ -87,6 +90,21 @@ class Client():
 		# Format date string
 		formatted_date = f"{abbreviated_day_of_week}, {current_day}/{current_month}/{current_date.year}"
 		
-		#Retreive current date in a format that the api requires. 
-		#EXAMPLE: "Tue, 9/11/2001"
+
 		return formatted_date
+	
+	def get_diary_data(self, notification_id):
+		# Construct API endpoint URL
+		endpoint = "diaryDetails"
+		api_url = f"https://beaconlightacademy.edu.pk/app/restapi.php?endpoint={endpoint}&accessToken={self.token}&appUserNotificationId={notification_id}"
+
+		# Send POST request to API endpoint with headers
+		response = self.client.post(api_url, headers=self.headers)
+
+		#! Raise an error if POST fails
+		response.raise_for_status()
+
+		# Retreive diary data from JSON response
+		data = json.loads(response.content)['data']
+
+		return data
